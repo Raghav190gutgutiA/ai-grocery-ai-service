f641cc2441a9f6102e9eb9acf63a87d9d693aeb2
Raghav190gutgutiA <raghavgutgutia172@gmail.com>
2026-07-12 01:45:42 +0530

    cors

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ const {
 } = require("./socket/socket.server");
 
 const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(
